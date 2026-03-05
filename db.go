a70e63bd25e52f50903d173e1539b11183f33bf7
raj11kanani <rajkanani1112@gmail.com>
2026-03-05 11:34:29 +0530

    fix: enable SSL for RDS connection

--- db.go
+++ db.go
@@ -20,7 +20,7 @@ func initDB() {
 	dbname := getEnv("DB_NAME", "urlshortener")
 
 	connStr := fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=require",
 		host, port, user, password, dbname,
 	)
 
